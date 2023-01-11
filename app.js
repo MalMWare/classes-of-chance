cb124790988ab5d4670113816ca05f89a3129ead
MalMWare <malkelleymartin@gmail.com>
2023-01-11 01:19:32 -0800

    card game constructors finished

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 class Casino {
-    // Write code here
-};
+    constructor(name, fakeCoin){
+        this.timesPlayed = 0;
+        this.name = name;
+        this.fakeCoin = fakeCoin
+    }
+
+    playGame(betAmount) {
+        this.timesPlayed++;
+        if(Math.random() <= 0.5 || this.fakeCoin) {
+            console.log(this.name + ' wins')
+        } else {
+            console.log(`Player wins ${betAmount*(this.timesPlayed + 1)}`)
+        }
+    }
+    rollDie() {
+        
+    }
+}
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+ const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
@@ -11,20 +27,20 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
